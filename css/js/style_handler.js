d6d04b4e7129ca9fe0e71c955cc5916573a6c656
Victor Rivas <vctr90w@gmail.com>
2014-09-14 14:43:18 -0430

    Columms height fixed for showing in the right place

--- css/js/style_handler.js
+++ css/js/style_handler.js
@@ -1,26 +1,37 @@
 //console.log($('.post-link').children()[0]);
 //
+jQuery(window).load(function(){
 var l_height = 0;
 var relation;
 var r_height = 0;
+
+var $postsDivs = document.getElementById('match').parentNode.childNodes;
+var $ph = [];
+var j = 0;
+for(var i=3;i<=$postsDivs.length;i=i+4){
+	$ph[j] = $postsDivs[i].childNodes[1].clientHeight;
+	j++;
+}
 
 var post_list_place = function(){
 	$('.post-list').children().each(function(i){
 		if(i!==0){
 			relation = l_height - r_height;
 			if(relation <= 0){
-				//Left positioning
+				l_height += $ph[i];
 				$(this).children().addClass('left');
-				l_height += $(this).children().outerHeight();
 			}else{
-				//Rigth positioning
+				r_height += $ph[i];
 				$(this).children().addClass('right');
-				r_height += $(this).children().outerHeight();
+				console.log('-------------------------------');
 			}
 		}else{
+			console.log('-------------------------------');
+			l_height += $ph[i];
 			$(this).children().addClass('left');
-			l_height += $(this).children().outerHeight();
 		}
+console.log($(this).children().height());
+console.log($(this).children());
 	});
 	l_height=0;
 	r_height=0;
@@ -41,3 +52,4 @@ $(window).resize(function(){
 if($(window).width()>600){
 	post_list_place();
 }
+});
